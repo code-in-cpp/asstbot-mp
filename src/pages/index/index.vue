b8ad30f339d701e7da7c995e404c5ac84ce0fe7d
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-06-25 18:48:09 +0800

    first version

---
+++ src/pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="page">
+    <scroll-view class="content">
+
+    </scroll-view>
+    <view class="footer">
+      Test
+    </view>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+      motto: 'Hello World',
+      userInfo: {}
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+    bindViewTap () {
+      const url = '../logs/main'
+      wx.navigateTo({ url })
+    },
+    getUserInfo () {
+      // 调用登录接口
+      wx.login({
+        success: () => {
+          wx.getUserInfo({
+            success: (res) => {
+              this.userInfo = res.userInfo
+            }
+          })
+        }
+      })
+    },
+    clickHandle (msg, ev) {
+      console.log('clickHandle:', msg, ev)
+    }
+  },
+
+  created () {
+    // 调用应用实例的方法获取全局数据
+    this.getUserInfo()
+  }
+}
+</script>
+
+<style scoped>
+.page {
+  display: flex;
+  height: 100vh;
+  flex-direction: column;
+}
+
+.content {
+  flex: 1;
+  background-color: rgb(249, 249, 249)
+}
+
+.footer {
+  background-color: rgb(236, 232, 232)
+}
+
+</style>
