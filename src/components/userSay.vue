7c3321a747828c0d3f8dbf1cb60a4fe0f5e357b0
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-06-27 12:08:42 +0800

    add component

---
+++ src/components/userSay.vue
@@ -0,0 +1,27 @@
+<template>
+  <view class="wrapper">
+    <view class="outgoing">
+      {{content}}
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  props: ['content']
+}
+</script>
+
+<style scoped>
+.wrapper {
+  display: flex;
+  flex-direction: row-reverse;
+}
+
+.outgoing {
+  padding: 10px;
+  border: solid #2b85e4 1px;
+  border-radius: 10px 10px 0 10px;
+  background-color: #70b3f7
+}
+</style>
