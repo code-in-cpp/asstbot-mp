eec12b83c5e920edb2033d2b66e0eb939873fa63
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-06-27 12:13:00 +0800

    user profile backend update

--- src/config.js
+++ src/config.js
@@ -8,12 +8,11 @@ var host = 'https://xiaodamp.cn'
 var hostRoot = `${host}/asstbot`
 
 export default {
-
-  // 下面的地址配合云端 Demo 工作
   service: {
     host,
     // 登录地址，用于建立会话
     messageUrl: `${hostRoot}/message`,
-    openIdUrl: `${hostRoot}/openid`
+    openIdUrl: `${hostRoot}/openid`,
+    userInfoUrl: `${hostRoot}/user/wechat`
   }
 }
--- src/store/modules/userProfile.js
+++ src/store/modules/userProfile.js
@@ -1,3 +1,9 @@
+import wechat from './wechat'
+import config from '@/config.js'
+
+console.log(config)
+const url = config.service.userInfoUrl
+
 const state = {
   userInfo: {},
   authed: false
@@ -25,6 +31,25 @@ const actions = {
             wx.getUserInfo({
               success: function (res) {
                 console.log(res.userInfo)
+                wechat.getOpenId()
+                  .then((openid) => {
+                    wx.request({
+                      url,
+                      data: {
+                        id: openid,
+                        wechat: res.userInfo
+                      },
+                      method: 'POST',
+                      success: (response) => {
+                        console.log(response.data)
+                        commit('appendMessage', response.data)
+                        resolve(response)
+                      },
+                      fail: (err) => {
+                        reject(err)
+                      }
+                    })
+                  })
               }
             })
           } else {
