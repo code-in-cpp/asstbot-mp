090007427af231048db8cab65883f2877ee8c94a
WeiGangqiang <284894567@qq.com>
2018-07-08 13:38:57 +0800

    fix bug

---
+++ src/pages/chart/index.vue
@@ -0,0 +1,77 @@
+<template>
+    <view class="container">
+         <canvas canvas-id="pieCanvas" class="canvas" style="height:300px" bindtouchstart="touchHandler"></canvas>
+         <canvas canvas-id="columnCanvas" class="canvas" style="height:300px" bindtouchstart="touchHandler"></canvas>
+   </view>
+</template>
+
+
+<script>
+import WxCharts from '@/utils/wxcharts'
+/* eslint-disable */
+var pieChart = null
+export default {
+  data () {
+    return {
+    }
+  },
+
+  computed: {
+  },
+
+  methods: {
+  },
+
+  onLoad (option) {
+    var windowWidth = 320
+    try {
+      var res = wx.getSystemInfoSync()
+      windowWidth = res.windowWidth
+    } catch (e) {
+      console.error('getSystemInfoSync failed!')
+    }
+    pieChart = new WxCharts({
+      animation: true,
+      canvasId: 'pieCanvas',
+      type: 'pie',
+      series: [{
+        name: '成交量1',
+        data: 15
+      },
+      {
+        name: '成交量2',
+        data: 35
+      },
+      {
+        name: '成交量3',
+        data: 78
+      }],
+      width: windowWidth,
+      height: 300,
+      dataLabel: true
+    })
+
+    new WxCharts({
+        canvasId: 'columnCanvas',
+        type: 'column',
+        categories: ['2012', '2013', '2014', '2015', '2016', '2017'],
+        series: [{
+            name: '成交量1',
+            data: [15, 20, 45, 37, 4, 80]
+        }, {
+            name: '成交量2',
+            data: [70, 40, 65, 100, 34, 18]
+        }],
+        yAxis: {
+            format: function (val) {
+                return val + '万';
+            }
+        },
+        width: 320,
+        height: 200
+    });
+
+    console.log("comming here............")
+  }
+}
+</script>
