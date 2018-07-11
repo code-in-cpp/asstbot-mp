a300e449f6f58abd9617430b342067c83cf5b92e
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-07-11 11:34:48 +0800

    userSayCheckboxReply and usersay Radio Reply added

---
+++ src/components/userSay/userSayCheckboxReply.vue
@@ -0,0 +1,53 @@
+<template>
+  <view class="wrapper word-text right-block">
+    <view class="outgoing right-item">
+      {{displayText}}
+    </view>
+  </view>
+</template>
+
+<script>
+  export default {
+    props: {
+      content: {
+        type: Object,
+        default: {}
+      }
+    },
+    computed: {
+      displayText () {
+        return this.content.items.reduce((lhs, rhs) => {
+          return lhs.value + ',' + rhs.value
+        })
+      }
+    },
+    created () {
+    }
+  }
+</script>
+
+<style scoped>
+  .wrapper {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+  .word-text{
+    padding-bottom: 10px;
+  }
+  .outgoing {
+    padding:20rpx;
+    border:solid #81d4f4 1rpx;
+    border-radius:30rpx 30rpx 0 30rpx;
+    background-color:#b3e5fc;
+    font-size:28rpx;
+    margin-right:10rpx;
+    text-align:left;
+  }
+  .right-block{
+    display: flex;
+    flex-direction: row-reverse;
+  }
+  .right-item{
+    max-width: 70%;
+  }
+</style>
