98e6902fbdc6ec7174b399eb1ab2a6377ab4c21f
杨国伟 <951014483@qq.com>
2018-07-19 21:04:40 +0800

    修改bug

--- src/pages/surveyTitleAndIntro/index.vue
+++ src/pages/surveyTitleAndIntro/index.vue
@@ -3,14 +3,14 @@
     <title-bar title=" "/>
       <view class="content-box">
         <text class="content-text">标题</text>
-        <input class="content-input" type="text" autofocus="true" focus="true" @blur="editTitle" :value="survey.title">
+        <input class="content-input" type="text" autofocus="true" focus="true" @blur="editTitle" :value="title">
       </view>
       <view class="content-box">
         <text class="content-text">简介</text>
-        <input class="content-input" type="text" @blur="editIntro" :value="survey.intro">
+        <input class="content-input" type="text" @blur="editIntro" :value="intro">
       </view>
     <view class="btn-box">
-      <button class="btn-save" @click="saveSurvey">保存</button>
+      <button :disabled="!title" class="btn-save" @click="saveSurvey">保存</button>
       <!--<view>保存</view>-->
     </view>
     <home-button/>
@@ -20,32 +20,51 @@
 <script>
   import { mapState } from 'vuex'
   export default {
+    data () {
+      return {
+        title: {
+          type: String,
+          default: ''
+        },
+        intro: {
+          type: String,
+          default: ''
+        }
+      }
+    },
     computed: {
       ...mapState({
         survey: state => {
+          console.log('survey:' + state.currentSurvey.survey)
           return state.currentSurvey.survey
         }
       })
     },
     methods: {
       editTitle (e) {
-        if (e.mp.detail.value) {
-          this.$store.commit('updateSurveyTitle', e.mp.detail.value)
-        } else {
-          wx.showToast({title: '请输入标题'})
-        }
+        this.title = e.mp.detail.value
       },
       editIntro (e) {
-        this.$store.commit('updateSurveyIntro', e.mp.detail.value)
+        this.intro = e.mp.detail.value
       },
       saveSurvey () {
-        if (this.survey.title) {
+        if (this.title) {
+          this.$store.commit('updateSurveyTitle', this.title)
+          this.$store.commit('updateSurveyIntro', this.intro)
           this.$store.dispatch('editSurvey', this.survey)
             .then(() => {
               wx.navigateBack()
             })
+        } else {
+          wx.showToast({
+            title: '请输入有效标题'
+          })
         }
       }
+    },
+    onShow () {
+      this.title = this.survey.title
+      this.intro = this.survey.intro
     }
   }
 </script>
