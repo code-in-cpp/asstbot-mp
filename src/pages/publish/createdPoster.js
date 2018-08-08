10bc17e160a0dc6b3fe89cb37c71840d264e1681
jqxu <jing.q.xu@hotmail.com>
2018-08-08 16:21:53 +0800

    tune publish logic

--- src/pages/publish/createdPoster.js
+++ src/pages/publish/createdPoster.js
@@ -2,61 +2,60 @@ export class CreatedPoster {
   title = ''
   intro = ''
   shareQrCode = ''
+  avatar = '../image/avatar.png'
 
-  do (title, intro, shareQrCode) {
+  do (title, intro, shareQrCode, avatar) {
     this.title = title
     this.intro = intro
     this.shareQrCode = shareQrCode
-    // console.log('title:', this.title)
-    // console.log('intro:', this.intro)
-    // console.log('shareQrCode:', this.shareQrCode)
+    this.avatar = avatar
     return this._template()
   }
   _template () {
     return ({
       background: '../image/created-poster.png',
-      width: '470rpx',
-      height: '842rpx',
+      width: '600rpx',
+      height: '1068rpx',
       views: [{
         type: 'image',
         url: this.shareQrCode,
         // url: 'https://xiaodamp.cn/asstbot/image?name=29995920955311e88601e133de4e1e2f.png',
         css: {
-          left: '80rpx',
-          top: '340rpx',
-          width: '300rpx',
-          height: '300rpx'
+          left: '210rpx',
+          top: '810rpx',
+          width: '180rpx',
+          height: '180rpx'
         }
-      },
-      {
-        type: 'text',
-        text: this.title,
-        // text: '你是一个有趣的人吗？献给有趣的灵魂们。你是一个有趣的人吗？献给有趣的灵魂们。你是一个有趣的',
-        css: {
-          left: '30rpx',
-          top: '180rpx',
-          fontSize: '28rpx',
-          lineHeight: '36rpx',
-          align: 'left',
-          maxLines: 3,
-          width: '400rpx',
-          color: '#4A4A4A'
-        }
-      },
-      {
-        type: 'text',
-        text: this.intro,
-        // text: '好看的皮囊千篇一律，有趣的灵魂万里挑一。让你好看的皮囊见识你灵魂的有趣。好看的皮囊千篇一律，有趣的灵魂万里挑一。让你好看的皮囊见识你灵魂的有趣。好看的皮囊千篇一律，有趣的灵魂万里挑一。让你好看的皮囊见识你灵魂的有趣。',
-        css: {
-          left: '30rpx',
-          top: '650rpx',
-          fontSize: '22rpx',
-          lineHeight: '30rpx',
-          align: 'left',
-          width: '400rpx',
-          maxLines: 4,
-          color: '#6A6A6A'
-        }
+      // },
+      // {
+      //   type: 'text',
+      //   text: this.title,
+      //   // text: '你是一个有趣的人吗？献给有趣的灵魂们。你是一个有趣的人吗？献给有趣的灵魂们。你是一个有趣的',
+      //   css: {
+      //     left: '30rpx',
+      //     top: '180rpx',
+      //     fontSize: '28rpx',
+      //     lineHeight: '36rpx',
+      //     align: 'left',
+      //     maxLines: 3,
+      //     width: '400rpx',
+      //     color: '#4A4A4A'
+      //   }
+      // },
+      // {
+      //   type: 'text',
+      //   text: this.intro,
+      //   // text: '好看的皮囊千篇一律，有趣的灵魂万里挑一。让你好看的皮囊见识你灵魂的有趣。好看的皮囊千篇一律，有趣的灵魂万里挑一。让你好看的皮囊见识你灵魂的有趣。好看的皮囊千篇一律，有趣的灵魂万里挑一。让你好看的皮囊见识你灵魂的有趣。',
+      //   css: {
+      //     left: '30rpx',
+      //     top: '650rpx',
+      //     fontSize: '22rpx',
+      //     lineHeight: '30rpx',
+      //     align: 'left',
+      //     width: '400rpx',
+      //     maxLines: 4,
+      //     color: '#6A6A6A'
+      //   }
       }
       ]
     })
