24b30876cd327c2f29f32db94435e282b91bb4ba
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-08-18 17:27:35 +0800

    两种audio的显示

---
+++ static/components/botSay/audio/index.js
@@ -0,0 +1,33 @@
+// static/components/botSay/audio/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    current: {
+      poster: '',
+      name: '',
+      author: '',
+      src: ''
+    },
+    audioAction: {
+      method: 'pause'
+    }
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    audioPlayed: function (e) {
+      console.log('audio is played')
+    }
+  }
+})
