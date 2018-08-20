7e2dd2dbebf2b6c63a4e124ffbcfbefe9d9193e1
Zhao Yonggang <yonggang.zhao@icloud.com>
2018-08-20 15:06:45 +0800

    video add

---
+++ src/components/widget/videoplayer.vue
@@ -0,0 +1,49 @@
+<template>
+<view class="video-wrapper">
+  <view id="myvideo" style="postion:relative">
+    <video :src="src">
+    </video>
+    <view class="video-mask" @click="$emit('videoEnded')">
+    </view>
+  </view>
+</view>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      videoContext: {}
+    }
+  },
+  props: ['src'],
+  onLoad () {
+    this.videoContext = wx.createVideoContext('myVideo')
+  }
+}
+</script>
+
+<style>
+  .video-wrapper {
+    width: 100%;
+    height: 100vh;
+    position: fixed;
+    text-align: center;
+    z-index: 2;
+  }
+
+  .video-wrapper video {
+    margin-top: 300rpx;
+    z-index: 3000;
+  }
+
+  .video-wrapper .video-mask {
+    position:absolute;
+    background: rgba(90, 90, 90, 0.5);
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: 2000;
+  }
+</style>
---
+++ static/components/botSay/video/index.js
@@ -0,0 +1,25 @@
+// static/components/botSay/video/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    playVideo (event) {
+      this.triggerEvent('videoPlay', 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400')
+    }
+  }
+})
